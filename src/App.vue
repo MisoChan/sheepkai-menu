9fc5af971980b77eeeab0aea39a034af4fd4ae0e
N.Kishino <naopon2009@gmail.com>
2022-04-09 13:13:56 +0900

    それなりに初期画面

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
 <template>
   <router-view />
 </template>
+<style lang="scss" scoped src="@/assets/sass/nomalize.scss"></style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="main_menu">aaaa</div>
+</template>
+<style lang="scss" scoped src="@/assets/sass/main_menu.scss"></style>
+<script>
+export default {
+  name: "MainMenu",
+  props: {
+    msg: String,
+  },
+};
+</script>
