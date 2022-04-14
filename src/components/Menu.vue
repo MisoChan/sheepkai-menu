1256c8093aec95e9ac56874049c0e3ec819df3a9
N.Kishino <naopon2009@gmail.com>
2022-04-14 11:07:32 +0900

    あとはCSSかな

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,54 @@
 <template>
   <div class="main_menu">
-    <div class="contents"></div>
-    <div id="all_rights">© 2022 N.Kishino. All Rights Reserved.</div>
+    <div id="menu_contents">
+      <div v-for="item in menu_contents" :key="item.content_id">
+        <MenuContents :item="item"></MenuContents>
+      </div>
+    </div>
+    <div id="all_rights">© 2022 Nano-Shiki All Rights Reserved.</div>
   </div>
 </template>
 <style lang="scss" scoped src="@/assets/sass/main_menu.scss"></style>
 <script>
+import MenuContents from "@/components/MenuContents.vue";
 export default {
   name: "MainMenu",
+  components: {
+    MenuContents,
+  },
   props: {
     msg: String,
+  },
+  data: function () {
+    return {
+      menu_contents: [
+        {
+          imagesrc: "@/static",
+          contents_title: "What's New?",
+          link: "whats_new",
+        },
+        {
+          imagesrc: "@/static",
+          contents_title: "それシス",
+          link: "blog",
+        },
+        {
+          imagesrc: "@/static",
+          contents_title: "Tools",
+          link: "tools",
+        },
+        {
+          imagesrc: "@/static",
+          contents_title: "About Us",
+          link: "about",
+        },
+        {
+          imagesrc: "@/static",
+          contents_title: "Contact",
+          link: "contact",
+        },
+      ],
+    };
   },
 };
 </script>
--- src/components/MenuContents.vue
+++ src/components/MenuContents.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="menu_icon_frame"><img @src="imagesrc" /></div>
-  <div class="menu_contents_name">
-    <a>{{ contents_title }}</a>
+  <div class="menu_contents">
+    <div class="menu_icon_frame"><img @src="item.imagesrc" /></div>
+    <div class="menu_contents_name">
+      <router-link :to="item.link">{{ item.contents_title }}</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    imagesrc: {
-      Type: String,
-    },
-    contents_title: {
-      Type: String,
+    item: {
+      Type: Object,
+      Require: true,
     },
   },
 };
