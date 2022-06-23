8f8008125fac1e452046a7c5dbfd392c117f475d
N.Kishino <naopon2009@gmail.com>
2022-06-23 17:07:29 +0900

    MDヘッダーリスト取得ルーチン追加

--- src/script/MarkdownUtility.js
+++ src/script/MarkdownUtility.js
@@ -11,6 +11,13 @@ export class MarkDownUtility {
   }
   getParsedDOM(mdtext, isSanitize) {
     const convertedHtmlText = this.convertToHTML(mdtext, isSanitize);
+    let h2_id_number = 0;
+    const h2collection = convertedHtmlText.getElementsByTagName("h2");
+    // ここでH2に通しIDを振る
+    Array.from(h2collection).map((row) => {
+      row.id = "h2_index_" + h2_id_number;
+      h2_id_number = h2_id_number + 1;
+    });
     return new DOMParser().parseFromString(convertedHtmlText, "text/html");
   }
   convertImageSrc(mdtext, isSanitize, urlHeader) {
@@ -26,5 +33,20 @@ export class MarkDownUtility {
   getRemovedURLOriginFromElementSrc(element) {
     let url = new URL(element.src).toString();
     return url.replace(new URL(element.src).origin, "");
+  }
+
+  getMarkDownHeaderList(mdtext) {
+    //HTML文字列からDOMパーサにかけてHTMLとして処理できるようにする;
+    const parsedhtml = this.getParsedDOM(mdtext, true);
+    const h2collection = parsedhtml.getElementsByTagName("h2");
+    // 取得できたH2の配列を指定のObjectに変換する
+    const h2array = Array.from(h2collection).map((row) => {
+      let result = {
+        id: row.id,
+        title: row.textContent,
+      };
+      return result;
+    });
+    return h2array;
   }
 }
