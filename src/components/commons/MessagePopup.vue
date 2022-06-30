e56e1dd87724cfe8d433f413db6774a4267c8779
oyaki <whitemisosoup2001@gmail.com>
2022-06-30 23:32:48 +0900

    ポップアップ定義

---
+++ src/components/commons/MessagePopup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="popup_background">
+    <div class="popup_window_wrapper">
+      <div class="popup_window">
+        <div class="popup_title"></div>
+        <div class="popup_description"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    MessageID: {
+      Type: String,
+      Require: true,
+    },
+  },
+};
+</script>
