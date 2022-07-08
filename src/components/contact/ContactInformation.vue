c38bffbe286d68c590fa86bd1fc09576a1668d65
N.Kishino <naopon2009@gmail.com>
2022-07-08 18:36:38 +0900

    連絡先ページ完成

--- src/components/contact/ContactInformation.vue
+++ src/components/contact/ContactInformation.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="contact_information">
     <p class="contact_name">{{ mediaData.name }}</p>
-    <div v-for="media in mediaData.contactMedia" :key="media.type">
+    <div>{{ getRoleString(mediaData.role).join(",") }}</div>
+    <div>{{ getContactTitle(mediaData.contact_title) }}</div>
+    <div
+      v-for="media in mediaData.contactMedia"
+      :key="media.type"
+      class="media_line_wrapper"
+    >
       <div class="media_line">
-        <img :src="getMediaTypeIconURL(media.type)" class="media_icon" />
         <a
           :href="media.url"
           target="_blank"
           rel="noopener noreferrer"
           class="media_link"
-          >{{ media.name }}</a
+        >
+          <img :src="getMediaTypeIconURL(media.type)" class="media_icon" />
+          {{ media.name }}</a
         >
       </div>
     </div>
@@ -21,6 +28,7 @@
   src="@/assets/sass/components/contact/contact_information.scss"
 />
 <script>
+import SNSInfo from "@/json/SNSInformation.json";
 export default {
   props: {
     mediaData: {
@@ -29,7 +37,17 @@ export default {
     },
   },
   methods: {
-    getMediaTypeIconURL() {},
+    getMediaTypeIconURL(type) {
+      return SNSInfo[type].logo;
+    },
+    getRoleString(roles) {
+      return roles.map((data) => {
+        return this.$translate("Contacts", "role")[data];
+      });
+    },
+    getContactTitle(key) {
+      return this.$translate("Contacts", "contact_title")[key];
+    },
   },
 };
 </script>
