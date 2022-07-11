c182b113ffa5d2a7aa04d7729fce1210f23892be
N.Kishino <naopon2009@gmail.com>
2022-07-12 05:35:39 +0900

    Port設定

--- scripts/stage_sheepkainet.sh
+++ scripts/stage_sheepkainet.sh
@@ -3,4 +3,4 @@ docker stop sheepkai-menu-front-stage || true && docker rm sheepkai-menu-front-s
 cp ./Dockerfile ./Dockerfile_prod
 cp ./Dockerfile_stage ./Dockerfile
 docker image build  --no-cache --force-rm=true  -t sheepkai-menu-front:stage ./ 
-docker run -itd --rm=true --name=sheepkai-menu-front-stage sheepkai-menu-front:stage
+docker run -itd --rm=true --name=sheepkai-menu-front-stage -p 65080:80 sheepkai-menu-front:stage
