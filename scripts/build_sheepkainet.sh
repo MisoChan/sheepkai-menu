441521ede60cbc0dc79305e91260690ef1508f92
N.Kishino <naopon2009@gmail.com>
2022-07-12 12:03:24 +0900

    ファイル名変更

--- scripts/build_sheepkainet.sh
+++ scripts/build_sheepkainet.sh
@@ -1,3 +1,4 @@
 #!bin/bash
 
 docker image build  --no-cache --force-rm=true -t sheepkai-menu-front:latest ./ 
+docker run -itd --rm=true --name=sheepkai-menu-front-stage -p 64080:80 sheepkai-menu-front:latest
--- scripts/test_sheepkainet.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-cp ./Dockerfile ./Dockerfile_prod
-cp ./Dockerfile_stage ./Dockerfile
-docker image build  --no-cache --force-rm=true --target build-stage -t sheepkai-menu-front:test ./ 
-docker run --rm=true --name=sheepkai-menu-front-test sheepkai-menu-front:test 
