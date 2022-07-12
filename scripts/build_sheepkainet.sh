db3cfeec00eee41203f89e8bb92397de68b5349b
N.Kishino <naopon2009@gmail.com>
2022-07-12 12:47:56 +0900

    不要となったファイルの削除

--- scripts/build_sheepkainet.sh
+++ scripts/build_sheepkainet.sh
@@ -2,6 +2,5 @@
 
 
 docker stop "sheepkai-menu-front-"$1 || true && docker rm "sheepkai-menu-front-"$1 || true
-bash cp_dockerfile.sh $1
 docker image build  --no-cache --force-rm=true --target deploy-stage -t "sheepkai-menu-front:"$1 ./ 
 docker run -itd --rm=true --name="sheepkai-menu-front-"$1 -p $2":80" "sheepkai-menu-front:"$1
--- scripts/cp_dockerfile.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-cp ./Dockerfile ./Dockerfile_prod
-cp ./"Dockerfile_"$1 ./Dockerfile
