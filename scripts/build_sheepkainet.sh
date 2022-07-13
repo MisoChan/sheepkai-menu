ffc69a98ceab4af4808f3f821bf6d7bdba5befe6
N.Kishino <naopon2009@gmail.com>
2022-07-13 18:22:41 +0900

    手直し

--- scripts/build_sheepkainet.sh
+++ scripts/build_sheepkainet.sh
@@ -1,6 +1,9 @@
 #!bin/bash
-pwd
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+# カレントを絶対パスベースでソース本体に移動
+cd SCRIPT_DIR;cd ../
 docker stop "sheepkai-menu-front-"$1 || true && docker rm "sheepkai-menu-front-"$1 || true
+
 cp ".env.branch-"$1 ".env.production"
 docker image build  --no-cache --force-rm=true --target deploy-stage -t "sheepkai-menu-front:"$1  "--build-arg=production" ./ 
 docker run -itd --rm=true --name="sheepkai-menu-front-"$1 -p $3":80" --net "sheepkai-docker-network" "sheepkai-menu-front:"$1
