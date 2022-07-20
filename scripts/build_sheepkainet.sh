24f9982945acdb30e9b962e451071fc160e9a7a6
N.Kishino <naopon2009@gmail.com>
2022-07-20 22:45:08 +0900

    スクリプト修正

--- scripts/build_sheepkainet.sh
+++ scripts/build_sheepkainet.sh
@@ -6,4 +6,4 @@ docker stop "sheepkai-menu-front-"$1 || true && docker rm "sheepkai-menu-front-"
 
 cp ".env.branch-"$1 ".env.production"
 docker build  --no-cache --force-rm=true --target deploy-stage -t "sheepkai-menu-front:"$1  "--build-arg=production" ./ 
-docker run -itd --rm=true --name="sheepkai-menu-front-"$1 -p $3":80" --net "sheepkai-docker-network" "sheepkai-menu-front:"$1
+docker run -itd --restart=always --read-only=true --name="sheepkai-menu-front-"$1 -p $3":80"  --net "sheepkai-docker-network" "sheepkai-menu-front:"$1
