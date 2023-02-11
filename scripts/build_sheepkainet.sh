2155757c6849df0fc01883fd15d18546b42d8cd7
oyaki <whitemisosoup2001@gmail.com>
2023-02-11 20:22:18 +0900

    環境変更反映

--- scripts/build_sheepkainet.sh
+++ scripts/build_sheepkainet.sh
@@ -4,6 +4,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR;cd ../
 docker stop "sheepkai-menu-front-"$1 || true && docker rm "sheepkai-menu-front-"$1 || true
 
-cp ".env.branch-"$1 ".env.production"
 docker build  --no-cache --force-rm=true --target deploy-stage -t "sheepkai-menu-front:"$1  "--build-arg=production" ./ 
 docker run -itd --restart=always --read-only --name="sheepkai-menu-front-"$1 -v /var/cache/nginx -v /var/run -v /etc/nginx/conf.d -p $3":80"  --net "sheepkai-docker-network" "sheepkai-menu-front:"$1
