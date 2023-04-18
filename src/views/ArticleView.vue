c8a67e2bb47976aaa0776c8256a559bd76f7a017
oyaki <whitemisosoup2001@gmail.com>
2023-04-19 03:09:26 +0900

    例外抑制の方法変更

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -129,9 +129,10 @@ export default {
         this.$store.commit("setPageTitle", this.article_property.article_title);
         this.pageLoaded = true;
       } catch (exception) {
-        const error_code = !exception.response.data
-          ? "ERR_NETWORK"
-          : exception.response.data.status;
+        const error_code =
+          !exception.response || !exception.response.data
+            ? "ERR_NETWORK"
+            : exception.response.data.status;
         this.$refs.popup.openModal(error_code);
       }
     },
